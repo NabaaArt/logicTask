50801523ac79d879fa83ad85bacb375ca6910437
NabaaArt <103404642+NabaaArt@users.noreply.github.com>
2023-11-22 23:11:36 +0300

    final editing

--- logictask/src/App.jsx
+++ logictask/src/App.jsx
@@ -6,7 +6,7 @@ import { Modal } from "react-responsive-modal";
 import { useAppStore } from "./store";
 
 const EditModal = () => {
-  const { open, setOpen, title, price, setTitle, setPrice, id,category } = useAppStore();
+  const { open, setOpen, title, price, setTitle, setPrice, id, category } = useAppStore();
   const [loading, setLoading] = useState(false);
 
   const handleSave = () => {
@@ -51,9 +51,55 @@ const EditModal = () => {
       />
       <br />
       <br />
-      <button onClick={() => handleSave()}>{loading ? "Lodaing...": "Save"}</button>
+      <button onClick={() => handleSave()}>{loading ? "Lodaing..." : "Save"}</button>
     </Modal>
   );
+};
+const AddModal = () => {
+
+  const { open, setOpen, title, price, setTitle, setPrice } = useAppStore();
+  const [loading, setLoading] = useState(false);
+
+
+  const handleSave = () => {
+    setLoading(true);
+    fetch(`https://dummyjson.com/products/`, {
+      method: "PUT" /* or PATCH */,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title,
+        price,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setLoading(false);
+        setOpen(false)
+        alert(`${title} is added`);
+      });
+  };
+
+  return (
+    <Modal open={open} onClose={() => setOpen(false)} center>
+      <h3>Add product</h3>
+      <input
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <br />
+      <br />
+      <input
+        placeholder="Price"
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+      />
+      <br />
+      <br />
+      <button onClick={() => handleSave()}>{loading ? "Lodaing..." : "added"}</button>
+    </Modal>
+  );
+
 };
 
 function App() {
@@ -61,41 +107,52 @@ function App() {
   const { setOpen, setTitle, setPrice, setID } = useAppStore();
 
   const loadProducts = async () => {
-    fetch("https://dummyjson.com/products")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-      });
-
- 
+    try {
+      let resp = await fetch("https://dummyjson.com/products");
+      let data = await resp.json();
+      setProducts(data.products);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const deleteProduct = async (productID) => {
-    fetch(`https://dummyjson.com/products/${productID}`, {
-      method: "DELETE",
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.isDeleted === true) {
-          alert("Delete Successfull!");
-        }
+    try {
+      let resp = await fetch(`https://dummyjson.com/products/${productID}`, {
+        method: "DELETE",
       });
+      let data = await resp.json();
+      if (data.isDeleted === true) {
+        alert(`${productID} is Deleted`);
+      }
+    } catch (error) {
+      console.log(error);
+    };
 
-   
-   
+     
   };
-const addProduct=()=>{
+  const addProduct = () => {
 
-}
+    fetch('https://dummyjson.com/products/add', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        title:"added one  item",
+        /* other product data */
+      })
+    })
+      .then(res => res.json())
+      .then(console.log);
+  }
+
   useEffect(() => {
     loadProducts();
   }, []);
 
   return (
     <div>
-     <button className="add-btn" onClick={() => addProduct()}>Add</button>
+      <button className="add-btn" onClick={() => addProduct()}>Add</button> 
       <table>
-        
         <thead >
           <tr>
             <th>The Product ID</th>
@@ -114,7 +171,7 @@ const addProduct=()=>{
               <td>{el.category}</td>
               <td>
                 <button
-                className="edit-btn"
+                  className="edit-btn"
                   onClick={() => {
                     setOpen(true);
                     setID(el.id);
@@ -131,6 +188,7 @@ const addProduct=()=>{
         </tbody>
       </table>
       <EditModal />
+      <AddModal />
     </div>
   );
 }
